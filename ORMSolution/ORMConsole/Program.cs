ebca54ed6c3fb5963064349b8b0f1263a5426cae
Rick van der Laan <rick.laan92@gmail.com>
2020-05-26 21:54:56 +0200

    Empty solution.

---
+++ ORMSolution/ORMConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ORMConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
