1d8d57e9324e985f83a2335f13cd06eab1441f8e
Rick van der Laan <rick.laan92@gmail.com>
2020-07-10 17:26:30 +0200

    Added ORMEntityTests with a BasicFetch test.

---
+++ ORMSolution/ORMNUnit/SQL/ORMEntityTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using ORM;
+using ORM.Attributes;
+using ORMFakeDAL;
+
+namespace ORMNUnit.SQL
+{
+    [ORMUnitTest]
+    public class ORMEntityTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // Hack to force load the ORMFakeDAL assembly since the ORM has no clue of its existance.
+            new User();
+            // ¯\_(ツ)_/¯
+
+            new ORMInitialize();
+        }
+
+        [Test]
+        public void BasicFetch()
+        {
+            var expectedQuery = "SELECT TOP (1) * FROM [DBO].[USERS] AS [U] WHERE ([U].[ID] = @PARAM1);";
+
+            User user = new User(1);
+
+            Assert.AreEqual(expectedQuery, user.ExecutedQuery);
+        }
+    }
+}
